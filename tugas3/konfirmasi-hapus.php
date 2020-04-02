b8f9be69a43c660694852861419f8bd621f73d99
juliansyahrifqi <rifqipratama9@gmail.com>
2020-04-02 20:29:24 +0700

    fix beres

--- tugas3/konfirmasi-hapus.php
+++ tugas3/konfirmasi-hapus.php
@@ -21,24 +21,24 @@
     $query = mysqli_query($koneksi, "SELECT * from gitar WHERE id_gitar=$id_gitar LIMIT 1;");
 ?>
 
-<div class="container-barang">
-    <form action="proses-hapus.php?id_gitar=<?php echo $id_gitar ?>" method="post">
-        <?php
-            while($res = mysqli_fetch_assoc($query)) {
-                echo "<div class='card'>
-                        <img src='img/".$res['gambar']."'/>
-                        <div class='keterangan-gambar'>
-                            <h2>".$res['nama_gitar']."</h2>
-                            <p> ".$res['pick_up']." </p>
-                            <p> ".$res['buatan']." </p>
-                            <p> Rp. ".$res['harga']."</p>
-                        </div>    
-                    </div>";
-            }       
-        ?>   
-    <input type="submit" value="Konfirmasi Hapus">
-    </form>
-</div> 
+    <div class="container-barang">
+        <form action="proses-hapus.php?id_gitar=<?php echo $id_gitar ?>" method="post">
+            <?php
+                while($res = mysqli_fetch_assoc($query)) {
+                    echo "<div class='card'>
+                            <img src='img/".$res['gambar']."'/>
+                            <div class='keterangan-gambar'>
+                                <h2>".$res['nama_gitar']."</h2>
+                                <p> ".$res['pick_up']." </p>
+                                <p> ".$res['buatan']." </p>
+                                <p> Rp. ".$res['harga']."</p>
+                            </div>    
+                        </div>";
+                }       
+            ?>   
+        <input type="submit" value="Konfirmasi Hapus">
+        </form>
+    </div> 
     
 </body>
 </html>
